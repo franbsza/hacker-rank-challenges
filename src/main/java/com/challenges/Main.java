2b1b0a06a8b4b31b87643b98e6d0199132bcf410
franbsza <franciele.bsza@gmail.com>
2023-04-21 09:38:05 -0300

    refactorin main.java

--- src/main/java/com/challenges/Main.java
+++ src/main/java/com/challenges/Main.java
@@ -1,15 +1,5 @@
 package com.challenges;
 
-import com.challenges.basic.IntoConditional;
-import com.challenges.basic.MiniMaxSum;
-import com.challenges.basic.PlusMinus;
-
-
 public class Main {
-    public static void main(String[] args) {
-
-        IntoConditional.executeTest();
-        PlusMinus.executeTest();
-        MiniMaxSum.executeTest();
-    }
+    public static void main(String[] args){}
 }
